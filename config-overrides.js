8d2a0fa3fb619ba413f6e5cb874fbf90bcd0a766
Manash athparia <athmanash@gmail.com>
2024-04-04 01:09:19 +0530

    Commented out production configuration in config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,10 @@
 const { override } = require('customize-cra');
 
 module.exports = override(
-    config => {
-        if (config.mode === 'production') {
-            config.output.publicPath = 'https://mywebsite.com/';
-        }
-        return config;
-    }
+    // config => {
+    //     if (config.mode === 'production') {
+    //         config.output.publicPath = 'https://mywebsite.com/';
+    //     }
+    //     return config;
+    // }
 );
